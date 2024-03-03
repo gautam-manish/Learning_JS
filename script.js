d481a75d3cd58d93f6723700ec515abb2f65c332
Manish Gautam <manishgtm123@gmail.com>
2024-03-03 18:14:27 +0545

    Learnt about variable scope

--- script.js
+++ script.js
@@ -247,3 +247,15 @@ function add(x,y){
     console.log(result);
 }
 add(2,3);
+
+function function1 (){
+    let x = 1;
+    console.log(x);
+}
+
+function function2 (){
+    let x = 2;
+    console.log(x);
+}
+function1();
+function2();
