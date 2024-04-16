0f4581106e2c388216b004e1070075c296b5d614
Manish Gautam <manishgtm123@gmail.com>
2024-04-16 15:59:20 +0545

    Learnt about Getters and Setters

--- script.js
+++ script.js
@@ -879,3 +879,107 @@ console.log(hwak.name);
 console.log(hwak.age);
 console.log(hwak.flySpeed);
 hwak.fly();
+
+// Getter and Setter
+
+class Rectangle {
+    constructor (width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if(newWidth > 0){
+            this._width = newWidth
+        }
+        else{
+            console.error("Width must be a positive number")
+        }
+    }
+
+    set height(newHeight){
+        if(newHeight > 0){
+            this._heigth = newHeight;
+        }
+        else{
+            console.error("Height must be a positive number")
+        }
+    }
+
+    get width(){
+        return this._width;
+    }
+    
+    get height(){
+        return this._heigth;
+    }
+
+    get area (){
+        return this._heigth * this._width
+    }
+}
+
+const rectangle = new Rectangle (3, 4);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(`Area is ${rectangle.area}`);
+
+class Person{
+    
+    constructor (firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName (newFirstName){
+        if(typeof newFirstName === "string" && newFirstName.length > 0){
+            this._firstName = newFirstName;
+        }
+        else{
+            console.error("First name must be a non-empty string");
+        }
+    }
+
+    set lastName (newLastName){
+        if(typeof newLastName === "string" && newLastName.length > 0){
+            this._lastName = newLastName;
+        }
+        else{
+            console.error("Last name must be a non-empty string");
+        }
+    }
+
+    set age(newAge){
+        if(typeof newAge === "number" && newAge >=0){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a non-negative number")
+        }
+    }
+
+    get firstName(){
+        return this._firstName;
+    }
+    
+    get lastName(){
+        return this._lastName;
+    }
+
+    get fullName(){
+        return this._firstName + " " + this._lastName;
+    }
+    
+    get age(){
+        return this._age;
+    }
+}
+
+const person = new Person ("Manish", "Gautam", 20);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
